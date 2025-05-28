5af6730fb8be5456cd4a259fcf2e5c690e741d17
David Richmond <d@verichmond.dev>
2025-05-28 05:39:37 +0000

    initial support for huidu hd-wf2 led matrix driver board

---
+++ ports/espressif/boards/huidu_hd_wf2/mpconfigboard.h
@@ -0,0 +1,21 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2019 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+// Micropython setup
+
+#define MICROPY_HW_BOARD_NAME       "Huidu HD-WF2"
+#define MICROPY_HW_MCU_NAME         "ESP32S3"
+
+#define MICROPY_HW_LED_STATUS (&pin_GPIO40)
+
+#define DEFAULT_UART_BUS_RX         (&pin_GPIO12)
+#define DEFAULT_UART_BUS_TX         (&pin_GPIO11)
+
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO42)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO41)
+
